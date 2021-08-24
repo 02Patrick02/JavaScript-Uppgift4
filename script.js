1b13e8b12e54106b63516bfe3f8f1e530492a7c0
02Patrick02 <paku02001@utb.vaxjo.se>
2021-08-24 12:49:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function init(){
 window.onload=init;
 
 function doCalculations(){
-
+    let area;
     let width;
     let lenght;
     let distance;
@@ -19,17 +19,27 @@ function doCalculations(){
     width = Number(input2Element.value);
 
 
-        area = lenght * width;
+    area = lenght * width;
     resultElement.innerHTML = "<p>Rektangelns area blir " + area + "m<sup>2</sup>.</p>";
 
-        area = 3.14159 * lenght * width / 4;
+    area = 3.14159 * lenght * width / 4;
     resultElement.innerHTML += "<p>Elliepsens area blir " + area + "m<sup>2</sup>.</p>";
 
+    
+    area = (lenght * 1.5) * (width + 3);
+    resultElement.innerHTML += "<p>Rektangelns area ökar med 50% bredd och 3 meter i längden där nya värdet blir: " + area + "m<sup>2</sup>.</p>";
+
+    distance = (lenght * width) / 2 / conv[1] * 100; 
+    resultElement.innerHTML += "<p> Triangelns area blir: " + distance + " " + unit[1] + "</p>";
+
     resultElement.innerHTML += "<p>Längden " + lenght + " meter blir: </p>";
+
     distance = lenght / conv[0] * 100;
     resultElement.innerHTML += "<p>" + distance + " " + unit[0] + "</p>";
+
     distance = lenght / conv[1] * 100;
     resultElement.innerHTML += "<p>" + distance + " " + unit[1] + "</p>";
+
     distance = lenght / conv[2] * 100;
     resultElement.innerHTML += "<p>" + distance + " " + unit[2] + "</p>";
 }
